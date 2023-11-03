64e5819658eefed9f70896dce4582ce7d5dec708
Danielfinx <daniel.e.san.1999@gmail.com>
2023-11-03 17:35:09 -0400

    Small Changes

--- php/conection.php
+++ php/conection.php
@@ -1,13 +1,14 @@
 <?php
+    $host = "localhost";
     $username = "root";
     $password = "";
     $database = "weblogger_crud_uneweb";
 
-    $link= mysqli_connect ("localhost", $username, $password);
+    $link= mysqli_connect ($host, $username, $password);
     mysqli_select_db ($link, $database);
 
     try {
-        $pdo = new PDO("mysql:host=localhost;database=$database", $username, $password);
+        $pdo = new PDO("mysql:host=$host;database=$database", $username, $password);
         // Set the PDO error mode to exception
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
